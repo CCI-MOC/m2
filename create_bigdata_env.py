2778e788fc7eebe729ce851589431866bfe9525b
User <surana.p@husky.neu.edu>
2016-03-02 23:11:37 -0500

    halfway done

--- create_bigdata_env.py
+++ create_bigdata_env.py
@@ -89,22 +89,108 @@ def attach_nodes_haas_proj(haas_url, project, node_list, debug = None, prehooks 
 
     return ret_obj
 
-
-def attach_to_project_network(haas_url, project, node_list,\
+#Nic name has to be 
+def attach_to_project_network(haas_url, node_nic_list_t,\
+        network, channel = "vlan/native",\
         debug = None, prehooks = None, post_hooks = None):
-    pass
+    ret_obj = list()
+    for node, nic in node_nic_list_t:
+        api = '/node/' + node + '/nic/' + nic + '/connect_network'
+        c_api = urlparse.urljoin(haas_url, api)
+        print c_api
+        body = {"network" : network, "channel" : channel}
+        haas_req = HaasRequest('post', body)
+        t_ret, t_hook_ret = call_haas(c_api, haas_req, debug)
+        ret_obj.append(HaasReturns(t_ret, t_hook_ret))       
+        if debug:
+            print {"url" : c_api, "node, nic" : node_nic_list_t}
 
-def add_to_project(haas_url, project, node_c, debug = None, prehooks \
+    return ret_obj
+
+def detach_from_project_network(haas_url, node_nic_list_t,\
+        network,\
+        debug = None, prehooks = None, post_hooks = None):
+    ret_obj = list()
+    print node_nic_list_t
+    for node, nic in node_nic_list_t:
+        api = '/node/' + node + '/nic/' + nic + '/detach_network'
+        c_api = urlparse.urljoin(haas_url, api)
+        print c_api
+        body = {"network" : network}
+        haas_req = HaasRequest('post', body)
+        t_ret, t_hook_ret = call_haas(c_api, haas_req, debug)
+        ret_obj.append(HaasReturns(t_ret, t_hook_ret))       
+        if debug:
+            print {"url" : c_api, "node, nic" : node_nic_list_t}
+
+    return ret_obj
+
+
+def add_to_project(haas_url, project, node_c,\
+        network, channel = "vlan/native", \
+        nic = 'enp130s0f0', \
+        debug = None, prehooks \
         = None, posthooks = None):
+    node_tuple_list =  list()
     if debug:
         print "node count : " + str(node_c)
     node_c = int(node_c)
     free_list = list_free_nodes(haas_url, debug).json
     if len(free_list) < node_c:
         raise Exception("count greater than available") 
-    return attach_nodes_haas_proj(haas_url, project, free_list[0:node_c], debug) 
+    attach_ret = attach_nodes_haas_proj(haas_url,\
+            project, free_list[0:node_c], debug)
+
+    for ret, node in zip(attach_ret, free_list[0:node_c]):
+        if ret.haas_ret.status_code  is not 404 or ret.haas_ret.status_code is not 409:
+            node_tuple_list.append((node, nic))
+        else:
+            pass 
+    a_net_ret = attach_to_project_network(\
+        haas_url, node_tuple_list, network,\
+        channel, \
+        debug)
+    return attach_ret, a_net_ret
+'''
+(haas_url, node_nic_list_t,\
+        network, nic, channel = "vlan/native",\
+        debug = None, prehooks = None, post_hooks = None):
+'''
+
+def detach_nodes(haas_url, project, node_list,\
+        debug = None, pre_hooks = None, post_hooks = None):
+    api = '/detach_node'
+    c_api = urlparse.urljoin(haas_url, 'project/' + project + api)
+    ret_obj = list()
+    for node in node_list:
+        body = {"node" : node}
+        haas_req = HaasRequest('post', body)
+        t_ret, t_hook_ret = call_haas(c_api, haas_req, debug)
+        ret_obj.append(HaasReturns(t_ret, t_hook_ret))       
+    if debug:
+        print {"url" : c_api, "node_list" : node_list}
+
+def query_project_nodes(haas_url, project, \
+                    debug = None, pre_hooks = None, post_hooks = None):
+    api = '/nodes'
+    c_api = urlparse.urljoin(haas_url, '/project/' + project +  api)
+    haas_req = HaasRequest('get', None)
+    if debug:
+        print c_api 
+    free_node_list, hook_ret = call_haas(c_api, haas_req)
+    return HaasReturns(free_node_list, hook_ret)   
+
+   
+    
 
 if __name__ == "__main__":
-    haas_ret =  add_to_project('http://127.0.0.1:7000/', 'bmi_infra', 2, debug = True)
-    for ret in haas_ret:
-        print ret
+    #print   list_free_nodes('http://127.0.0.1:7000/', debug = True)
+    haas_ret =  query_project_nodes('http://127.0.0.1:7000/', 'bmi_infra', debug = True)
+    tt = list()
+    for jj in haas_ret.json:
+        tt.append((jj, 'enp130s0f0'))
+    print tt
+    print detach_from_project_network('http://127.0.0.1:7000/', tt, 'bmi-provision', debug = True)
+    print detach_nodes('http://127.0.0.1:7000/', 'bmi_infra', haas_ret.json, debug = True)
+    print "############################################# CLEARED ##########################"
+    #t = add_to_project('http://127.0.0.1:7000/', 'bmi_infra',2,"bmi-provision", debug = True)
