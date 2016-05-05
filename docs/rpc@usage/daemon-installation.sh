d32c5194d5b1a2f139e4cbf0c420a0fa1eb408c5
anuj-rt <thakur.an@husky.neu.edu>
2016-05-05 18:05:41 -0400

    daemon-installation file changed to start the daemon

--- docs/rpc@usage/daemon-installation.sh
+++ docs/rpc@usage/daemon-installation.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #This gives appropriate permission to the nameserver python file, 
 #and nameserver-daemon.sh file.
-chmod 755 nameserver.py  
-sudo cp nameserver-daemon.sh /etc/init.d  
-sudo chmod +x nameserver-daemon.sh  
+chmod 755 nameserver.py
+sudo chmod +x nameserver-daemon.sh
+sudo cp nameserver-daemon.sh /etc/init.d
 sudo update-rc.d nameserver-daemon.sh defaults
+sudo /etc/init.d/nameserver-daemon.sh start
 
 #This gives appropriate permission to the rpcserver python file,
 #and rpcserver-daemon.sh file.
-chmod 755 rpcserver.py  
-sudo cp rpcserver-daemon.sh /etc/init.d  
-sudo chmod +x rpcserver-daemon.sh  
+chmod 755 rpcserver.py
+sudo chmod +x rpcserver-daemon.sh
+sudo cp rpcserver-daemon.sh /etc/init.d
 sudo update-rc.d rpcserver-daemon.sh defaults
+sudo /etc/init.d/rpcserver-daemon.sh start
