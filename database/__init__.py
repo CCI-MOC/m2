829db4597e377cbdfcdad2004bd69a4d12dc7779
Sourabh Bollapragada <chemistry.sourabh@yahoo.co.in>
2016-05-17 18:01:46 -0400

    Fixed imports in __init__

--- database/__init__.py
+++ database/__init__.py
@@ -1,4 +1,4 @@
 # Added here so that single import can be used whenever this package is used
 from db_connection import DatabaseConnection
-from image_repository import *
-from project_repository import *
+from image import *
+from project import *
