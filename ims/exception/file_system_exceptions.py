5a9d37fa6d65bfe7473bf4cbddc36fcfa5c2ca69
Sourabh Bollapragada <chemistry.sourabh@yahoo.co.in>
2016-06-01 07:35:47 -0400

    Finished test cases for rest calls

--- ims/exception/file_system_exceptions.py
+++ ims/exception/file_system_exceptions.py
@@ -9,7 +9,7 @@ class ImageNotFoundException(FileSystemException):
         self.name = name
 
     def __str__(self):
-        return self.name+" Not Found"
+        return self.name + " Not Found"
 
 
 # This exception should be raised if some connection issues occured when communicating with file system
@@ -24,7 +24,7 @@ class ImageBusyException(FileSystemException):
         self.name = name
 
     def __str__(self):
-        return self.name+" is Busy"
+        return self.name + " is Busy"
 
 
 # this exception should be raised when some operation is called on an image that has snapshots
@@ -33,7 +33,7 @@ class ImageHasSnapshotException(FileSystemException):
         self.name = name
 
     def __str__(self):
-        return self.name+" has Snapshots"
+        return self.name + " has Snapshots"
 
 
 # this exception should be raised when some operation requires that an image not exist in the filesytem
@@ -42,7 +42,7 @@ class ImageExistsException(FileSystemException):
         self.name = name
 
     def __str__(self):
-        return self.name+" Already Exists"
+        return self.name + " Already Exists"
 
 
 # this exception should be raised when the given image is not opened
@@ -51,7 +51,7 @@ class ImageNotOpenedException(FileSystemException):
         self.name = name
 
     def __str__(self):
-        return self.name+" should be opened before operation"
+        return self.name + " should be opened before operation"
 
 
 # this exception should be raised when function that is not supported is called
@@ -68,22 +68,20 @@ class ArgumentsOutOfRangeException(FileSystemException):
 
 # this exception should be raised when the config file passed is invalid
 class InvalidConfigArgumentException(FileSystemException):
-
-    def __init__(self,arg):
+    def __init__(self, arg):
         self.arg = arg
 
     def __str__(self):
-        return "Invalid "+self.arg+" argument in config file"
+        return "Invalid " + self.arg + " argument in config file"
 
 
 # this exception should be raised when the config file contains an invalid argument
 class MissingConfigArgumentException(FileSystemException):
-
-    def __init__(self,arg):
+    def __init__(self, arg):
         self.arg = arg
 
     def __str__(self):
-        return self.arg+" is incorrect in config file"
+        return self.arg + " is incorrect in config file"
 
 
 # this exception class is the abstract class for any ceph specific exceptions
--- ims/test_web_ims.py
+++ ims/test_web_ims.py
@@ -1,180 +1,171 @@
 import time
 import unittest
-import requests
-
 from operations import *
 
 
 class TestOperations(unittest.TestCase):
     def test_provision(self):
-
         url = "http://192.168.122.125:8000/BMI/default/"
 
-        data = {"node":"super-37","img":"i12","snap_name":"HadoopGoldenImage"}
-        res = requests.put(url+"provision_node/",data=data)
+        data = {"node": "super-37", "img": "i12",
+                "snap_name": "HadoopGoldenImage"}
+        res = requests.put(url + "provision_node/", data=data)
         print res.content
-        self.assertEqual(res.status_code,404)
+        self.assertEqual(res.status_code, 404)
 
         time.sleep(30)
 
-        data = {"node":"super-37"}
-        res = requests.delete(url+"remove_node/",data=data)
+        data = {"node": "super-37"}
+        res = requests.delete(url + "remove_node/", data=data)
         print res.content
-        self.assertEqual(res.status_code,500)
+        self.assertEqual(res.status_code, 500)
 
         time.sleep(30)
 
-        data = {"node":"super-37","img":"hadoopMaster.img","snap_name":"hello"}
-        res = requests.put(url+"provision_node/",data=data)
+        data = {"node": "super-37", "img": "hadoopMaster.img",
+                "snap_name": "hello"}
+        res = requests.put(url + "provision_node/", data=data)
         print res.content
-        self.assertEqual(res.status_code,404)
+        self.assertEqual(res.status_code, 404)
 
         time.sleep(30)
 
-        data = {"node":"super-37"}
-        res = requests.delete(url+"remove_node/",data=data)
+        data = {"node": "super-37"}
+        res = requests.delete(url + "remove_node/", data=data)
         print res.content
-        self.assertEqual(res.status_code,500)
+        self.assertEqual(res.status_code, 500)
 
         time.sleep(30)
 
-        data = {"node":"super-37","img":"hadoopMaster.img","snap_name":"HadoopMasterGoldenImage"}
-        res = requests.put(url+"provision_node/",data=data)
+        data = {"node": "super-37", "img": "hadoopMaster.img",
+                "snap_name": "HadoopMasterGoldenImage"}
+        res = requests.put(url + "provision_node/", data=data)
         print res.content
-        self.assertEqual(res.status_code,200)
+        self.assertEqual(res.status_code, 200)
 
-        data = {"node":"super-37","img":"hadoopMaster.img","snap_name":"HadoopMasterGoldenImage"}
-        res = requests.put(url+"provision_node/",data=data)
+        data = {"node": "super-37", "img": "hadoopMaster.img",
+                "snap_name": "HadoopMasterGoldenImage"}
+        res = requests.put(url + "provision_node/", data=data)
         print res.content
-        self.assertEqual(res.status_code,500)
+        self.assertEqual(res.status_code, 500)
 
         time.sleep(30)
 
-        data = {"node":"super-37","img":"hadoopMaster.img","snap_name":"HadoopMasterGoldenImage"}
-        res = requests.put(url+"provision_node/",data=data)
+        data = {"node": "super-37", "img": "hadoopMaster.img",
+                "snap_name": "HadoopMasterGoldenImage"}
+        res = requests.put(url + "provision_node/", data=data)
         print res.content
-        self.assertEqual(res.status_code,500)
+        self.assertEqual(res.status_code, 500)
 
-        data = {"node":"super-37"}
-        res = requests.delete(url+"remove_node/",data=data)
+        data = {"node": "super-37"}
+        res = requests.delete(url + "remove_node/", data=data)
         print res.content
-        self.assertEqual(res.status_code,200)
+        self.assertEqual(res.status_code, 200)
 
-        data = {"node":"super-37"}
-        res = requests.delete(url+"remove_node/",data=data)
+        data = {"node": "super-37"}
+        res = requests.delete(url + "remove_node/", data=data)
         print res.content
-        self.assertEqual(res.status_code,500)
+        self.assertEqual(res.status_code, 500)
 
     def test_create_snapshot(self):
         url = "http://192.168.122.125:8000/BMI/default/"
 
         print "create snapshot"
 
-        data = {"project":"bmi_penultimat","img":"HadoopMaster.img","snap_name":"blblb1"}
-        res = requests.put(url + "snap_image/",data=data)
+        data = {"project": "bmi_penultimat", "img": "HadoopMaster.img",
+                "snap_name": "blblb1"}
+        res = requests.put(url + "snap_image/", data=data)
         print res.content
-        self.assertEqual(res.status_code,500)
+        self.assertEqual(res.status_code, 500)
 
-        data = {"project":"bmi_penultimate","img":"HadoopMaster","snap_name":"blblb1"}
-        res = requests.put(url + "snap_image/",data=data)
+        data = {"project": "bmi_penultimate", "img": "HadoopMaster",
+                "snap_name": "blblb1"}
+        res = requests.put(url + "snap_image/", data=data)
         print res.content
-        self.assertEqual(res.status_code,404)
+        self.assertEqual(res.status_code, 404)
 
-        data = {"project":"bmi_penultimate","img":"hadoopMaster.img","snap_name":"blblb1"}
-        res = requests.put(url + "snap_image/",data=data)
+        data = {"project": "bmi_penultimate", "img": "hadoopMaster.img",
+                "snap_name": "blblb1"}
+        res = requests.put(url + "snap_image/", data=data)
         print res.content
-        self.assertEqual(res.status_code,404)
+        self.assertEqual(res.status_code, 404)
 
-        data = {"project":"bmi_penultimate","img":"HadoopMaster.img","snap_name":"blblb1"}
-        res = requests.put(url + "snap_image/",data=data)
+        data = {"project": "bmi_penultimate", "img": "HadoopMaster.img",
+                "snap_name": "blblb1"}
+        res = requests.put(url + "snap_image/", data=data)
         print res.content
-        self.assertEqual(res.status_code,200)
+        self.assertEqual(res.status_code, 200)
 
         print "list snapshots"
 
-        data = {"project":"bmi_penultimat","img":"HadoopMaster.img"}
-        res = requests.post(url + "list_snapshots/",data=data)
+        data = {"project": "bmi_penultimat", "img": "HadoopMaster.img"}
+        res = requests.post(url + "list_snapshots/", data=data)
         print res.content
-        self.assertEqual(res.status_code,500)
+        self.assertEqual(res.status_code, 500)
 
-        data = {"project":"bmi_penultimate","img":"HadoopMaster"}
-        res = requests.post(url + "list_snapshots/",data=data)
+        data = {"project": "bmi_penultimate", "img": "HadoopMaster"}
+        res = requests.post(url + "list_snapshots/", data=data)
         print res.content
-        self.assertEqual(res.status_code,404)
+        self.assertEqual(res.status_code, 404)
 
-        data = {"project":"bmi_penultimate","img":"hadoopMaster.img"}
-        res = requests.post(url + "list_snapshots/",data=data)
+        data = {"project": "bmi_penultimate", "img": "hadoopMaster.img"}
+        res = requests.post(url + "list_snapshots/", data=data)
         print res.content
-        self.assertEqual(res.status_code,404)
+        self.assertEqual(res.status_code, 404)
 
-        data = {"project":"bmi_penultimate","img":"HadoopMaster.img"}
-        res = requests.post(url + "list_snapshots/",data=data)
+        data = {"project": "bmi_penultimate", "img": "HadoopMaster.img"}
+        res = requests.post(url + "list_snapshots/", data=data)
         print res.content
-        self.assertEqual(res.status_code,200)
-        # self.assertEqual(res.content)
+        js = res.json()
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(js.__len__(),5)
+        self.assertEqual(js[4],'blblb1')
 
-    #     output = good_bmi.list_snaps('bmi_penultimate', 'HadoopMaster.img')
-    #     print output
-    #     self.assertEqual(output['status_code'], 200)
-    #     self.assertEqual(output['retval'].__len__(), 5)
-    #     self.assertEqual(output['retval'][4], 'blblb1')
-
-        data = {"project":"bmi_penultimate","img":"HadoopMaster.img","snap_name":"blblb1"}
-        res = requests.put(url + "snap_image/",data=data)
+        data = {"project": "bmi_penultimate", "img": "HadoopMaster.img",
+                "snap_name": "blblb1"}
+        res = requests.put(url + "snap_image/", data=data)
         print res.content
-        self.assertEqual(res.status_code,471)
+        self.assertEqual(res.status_code, 471)
 
         print "remove snapshots"
 
-        data = {"project":"bmi_penultimat","img":"HadoopMaster.img","snap_name":"blblb1"}
-        res = requests.delete(url + "remove_snapshot/",data=data)
+        data = {"project": "bmi_penultimat", "img": "HadoopMaster.img",
+                "snap_name": "blblb1"}
+        res = requests.delete(url + "remove_snapshot/", data=data)
         print res.content
-        self.assertEqual(res.status_code,500)
+        self.assertEqual(res.status_code, 500)
 
-        data = {"project":"bmi_penultimate","img":"HadoopMaster","snap_name":"blblb1"}
-        res = requests.delete(url + "remove_snapshot/",data=data)
+        data = {"project": "bmi_penultimate", "img": "HadoopMaster",
+                "snap_name": "blblb1"}
+        res = requests.delete(url + "remove_snapshot/", data=data)
         print res.content
-        self.assertEqual(res.status_code,404)
+        self.assertEqual(res.status_code, 404)
 
-        data = {"project":"bmi_penultimate","img":"hadoopMaster.img","snap_name":"blblb1"}
-        res = requests.delete(url + "remove_snapshot/",data=data)
+        data = {"project": "bmi_penultimate", "img": "hadoopMaster.img",
+                "snap_name": "blblb1"}
+        res = requests.delete(url + "remove_snapshot/", data=data)
         print res.content
-        self.assertEqual(res.status_code,404)
+        self.assertEqual(res.status_code, 404)
 
-        data = {"project":"bmi_penultimate","img":"HadoopMaster.img","snap_name":"blblb1"}
-        res = requests.delete(url + "remove_snapshot/",data=data)
+        data = {"project": "bmi_penultimate", "img": "HadoopMaster.img",
+                "snap_name": "blblb1"}
+        res = requests.delete(url + "remove_snapshot/", data=data)
         print res.content
-        self.assertEqual(res.status_code,200)
+        self.assertEqual(res.status_code, 200)
 
-    # def test_list_all_images(self):
-    #     good_bmi = BMI("haasadmin", "admin1234")
-    #     bad_bmi = BMI("haasadmin", "admin123##")
-    #
-    #     output = bad_bmi.list_all_images('bmi_penultimate')
-    #     print output
-    #     self.assertEqual(output['status_code'], 401)
-    #
-    #     output = good_bmi.list_all_images('bmi_penultimate')
-    #     print output
-    #     self.assertEqual(output['status_code'], 404)
-    #
-    #     pr = ProjectRepository()
-    #     pr.insert("bmi_penultimate", "bmi_provision")
-    #
-    #     output = good_bmi.list_all_images('bmi_penultimate')
-    #     print output
-    #     self.assertEqual(output['status_code'], 200)
-    #     self.assertEqual(output['retval'].__len__(), 0)
-    #     self.assertEqual(output['retval'], [])
-    #
-    #     imgr = ImageRepository()
-    #     imgr.insert("testimage", 1)
-    #
-    #     output = good_bmi.list_all_images('bmi_penultimate')
-    #     print output
-    #     self.assertEqual(output['status_code'], 200)
-    #     self.assertEqual(output['retval'].__len__(), 1)
-    #     self.assertEqual(output['retval'][0], 'testimage')
-    #
-    #     pr.delete_with_name("bmi_penultimate")
-        pass
+    def test_list_all_images(self):
+        url = "http://192.168.122.125:8000/BMI/default/"
+
+        data = {"project": "bmi_penultimat"}
+        res = requests.post(url + "list_images/", data=data)
+        print res.content
+        self.assertEqual(res.status_code, 500)
+
+        data = {"project": "bmi_penultimate"}
+        res = requests.post(url + "list_images/", data=data)
+        print res.content
+        js = res.json()
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(js.__len__(),2)
+        self.assertEqual(js[0],"hadoopMaster.img")
+        self.assertEqual(js[1],"HadoopMaster.img")
