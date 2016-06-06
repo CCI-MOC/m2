ca3e582ce2fb6daba233e860bffc9f94c32b72e9
Sourabh Bollapragada <chemistry.sourabh@yahoo.co.in>
2016-06-06 19:06:41 -0400

    Updated setup.py and folder structure

--- setup.py
+++ setup.py
@@ -1,10 +1,40 @@
+import sys
+
 from setuptools import setup
+
+args = sys.argv
+
+picasso_install_requires = ["pyro4", "requests"]
+einstein_install_requires = ["sqlachemy>=1.0.13", "requests", "pyro4"]
+picasso_packages = []
+einstein_packages = ['ims', 'ims.database',
+                     'ims.exception', 'ims.rpc.server']
+
+install_requires = []
+packages = []
+
+if args[1] == 'install':
+    if args[2] == 'einstein':
+        install_requires = einstein_install_requires
+        packages = einstein_packages
+    elif args[2] == 'picasso':
+        install_requires = picasso_install_requires
+        packages = picasso_packages
+    elif args[2] == 'einstein+picasso':
+        install_requires = list(
+            set(einstein_install_requires + picasso_install_requires))
+        packages = list(set(einstein_packages + picasso_packages))
+    else:
+        print "Invalid Argument"
+        sys.exit()
+
+    sys.argv.pop()
 
 setup(
     name='ims',
     version='0.2',
-    install_requires=["sqlalchemy>=1.0.13", 'requests'],
-    packages=['ims', 'ims.database', 'ims.exception'],
+    install_requires=install_requires,
+    packages=packages,
     url='',
     license='',
     author='chemistry_sourabh',
