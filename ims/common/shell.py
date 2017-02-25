3daaa8bd4ad2a71c4104caacb62bbddfa9a65886
chemistry_sourabh <chemistry.sourabh@yahoo.co.in>
2017-02-24 20:23:32 -0500

    Missed pushing this

--- ims/common/shell.py
+++ ims/common/shell.py
@@ -5,8 +5,14 @@
 import subprocess
 
 
-# Calls Command
 def call(command, sudo=False):
+    """
+    Executes the given command in shell
+
+    :param command: the command to execute as string
+    :param sudo: whether to execute as root (default is False)
+    :return: output of command as string
+    """
     s_command = command.split()
     if sudo:
         s_command.insert(0, 'sudo')
