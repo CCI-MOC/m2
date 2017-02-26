a6182d2551813be62a0a092d8513a5b3f1b09d8e
chemistry_sourabh <chemistry.sourabh@yahoo.co.in>
2017-02-26 02:05:59 -0500

    config changes

--- ims/common/config.py
+++ ims/common/config.py
@@ -9,9 +9,16 @@ from ims.common.bmi_config import parse_config
 __config = None
 
 
-def load():
+def load(force=False):
+    """
+    Loads the config by using BMI_CONFIG environment variable, else loads from
+    default location
+
+    :param force: Forces a reload of config
+    :return: None
+    """
     global __config
-    if __config is None:
+    if __config is None or force:
         try:
             path = os.environ[
                 constants.CONFIG_LOCATION_ENV_VARIABLE]
@@ -23,23 +30,43 @@ def load():
 
 
 def get():
+    """
+    Returns the global config
+
+    :return: The Global Config
+    """
     global __config
     return __config
 
 
 class __BMIConfig:
-    # other instance variables are initialized as to not get AttributeErrors
     def __init__(self, filename):
         self.configfile = filename
         self.config = None
 
     def load_config(self):
+        """
+        Loads the config as per the given path
+
+        :return: None
+        """
         config = ConfigParser.SafeConfigParser()
         if not config.read(self.configfile):
             raise IOError('cannot load ' + self.configfile)
         self.config = config
 
     def option(self, section, option, type=str, required=True):
+        """
+        Parses the given option from config, converts to another type if
+        required or raises an exception if found missing and adds it as
+        attribute to config as cfg.section.option
+
+        :param section: Section under which Option should be present
+        :param option: Option should be parsed
+        :param type: the conversion function for the required type like int,etc
+        :param required: Whether an exception should be raised if missing
+        :return: None
+        """
         try:
             value = self.config.get(section, option)
             section_obj = getattr(self, section, None)
@@ -47,15 +74,29 @@ class __BMIConfig:
                 section_obj = ConfigSection()
                 setattr(self, section, section_obj)
             if type == bool:
-                setattr(section_obj, option, value.lower() == 'true')
+                v = value.lower()
+                if v in ['true', 'false']:
+                    setattr(section_obj, option, v == 'true')
+                else:
+                    raise ValueError
             else:
                 setattr(section_obj, option, type(value))
         except ConfigParser.Error:
             if required:
                 raise config_exceptions.MissingOptionInConfigException(option,
                                                                        section)
+        except ValueError:
+            raise config_exceptions.InvalidValueConfigException(option,
+                                                                section)
 
     def section(self, section_name, required=True):
+        """
+        Parses entire section and adds them to config object like option
+
+        :param section_name: the section to parse
+        :param required: Whether an exception should be raised if missing
+        :return: None
+        """
         try:
             section = self.config.items(section_name)
             section_obj = getattr(self, section_name, None)
@@ -73,5 +114,6 @@ class __BMIConfig:
 
 
 class ConfigSection:
+    """ A Simple Object to be used to get cfg.section.option """
     def __init__(self):
         pass
--- ims/exception/config_exceptions.py
+++ ims/exception/config_exceptions.py
@@ -25,3 +25,17 @@ class MissingSectionInConfigException(ConfigException):
 
     def __str__(self):
         return "Missing " + self.option + " section in config file"
+
+
+class InvalidValueConfigException(ConfigException):
+    @property
+    def status_code(self):
+        return 500
+
+    def __init__(self, option, section):
+        self.option = option
+        self.section = section
+
+    def __str__(self):
+        return "Invalid Value for {0} option in section {1}".format(
+            self.option, self.section)
